a1e295f83da4addef65e0dfbd2bb9c2c4945b40b
Lhuqita Fazry <lhuqita.fazry@gmail.com>
2017-05-28 05:11:36 +0700

    Activate speech to text

--- app/src/main/java/id/co/rumahcoding/qaalam/MainActivity.java
+++ app/src/main/java/id/co/rumahcoding/qaalam/MainActivity.java
@@ -1,14 +1,22 @@
 package id.co.rumahcoding.qaalam;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.provider.BaseColumns;
+import android.speech.RecognizerIntent;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 import id.co.rumahcoding.qaalam.models.Entry;
 import io.realm.Case;
@@ -17,22 +25,24 @@ import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
     private SimpleCursorAdapter mAdapter;
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final SearchView searchView = (SearchView) findViewById(R.id.search_view);
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        ImageButton voiceImageButton = (ImageButton) findViewById(R.id.voice_button);
 
         final String[] from = new String[] {"text"};
         final int[] to = new int[] {android.R.id.text1};
 
         mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_dropdown_item,
                 null, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-        searchView.setSuggestionsAdapter(mAdapter);
+        mSearchView.setSuggestionsAdapter(mAdapter);
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
@@ -40,14 +50,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                final MatrixCursor matrixCursor = new MatrixCursor(new String[]{ BaseColumns._ID, "text" });
+                final MatrixCursor matrixCursor = new MatrixCursor(new String[]{
+                        BaseColumns._ID, "text"
+                });
 
-                //for (int i=0; i<strArrData.length; i++) {
-                //    if (strArrData[i].toLowerCase().startsWith(s.toLowerCase()))
-                //        mc.addRow(new Object[] {i, strArrData[i]});
-                //}
                 Realm realm = Realm.getDefaultInstance();
-                RealmResults<Entry> realmResults = realm.where(Entry.class).contains("tarjim", newText, Case.INSENSITIVE).findAll();
+                RealmResults<Entry> realmResults = realm.where(Entry.class)
+                        .contains("tarjim", newText, Case.INSENSITIVE).findAll();
 
                 for(int i = 0; i < realmResults.size(); i++) {
                     Entry entry = realmResults.get(i);
@@ -56,11 +65,12 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 mAdapter.changeCursor(matrixCursor);
-                return false;
+
+                return true;
             }
         });
 
-        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+        mSearchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
             @Override
             public boolean onSuggestionSelect(int position) {
                 return true;
@@ -68,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onSuggestionClick(int position) {
-                CursorAdapter ca = searchView.getSuggestionsAdapter();
+                CursorAdapter ca = mSearchView.getSuggestionsAdapter();
                 Cursor cursor = ca.getCursor();
                 cursor.moveToPosition(position);
                 long id = cursor.getLong(0);
@@ -81,5 +91,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        voiceImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "in_ID");
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                        "");
+                try {
+                    startActivityForResult(intent, 1);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Speech not supported",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 1: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    //txtSpeechInput.setText(result.get(0));
+                    mSearchView.setQuery(result.get(0), true);
+                }
+
+                break;
+            }
+
+        }
     }
 }
